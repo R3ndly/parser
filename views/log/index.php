9c67c3da78b1893c377a488608994aec671cd569
R3ndly <akalinart020@gmail.com>
2025-08-03 20:01:34 +0800

    Добавил страницу представления

--- views/log/index.php
+++ views/log/index.php
@@ -1,9 +1,144 @@
 <?php
-/** @var yii\web\View $this */
-?>
-<h1>log/index</h1>
+use yii\helpers\Html;
+use yii\grid\GridView;
+use yii\widgets\Pjax;
+use yii\widgets\ActiveForm;
+use app\models\Os;
+use dosamigos\highcharts\HighCharts;
 
-<p>
-    You may change the content of this page by modifying
-    the file <code><?= __FILE__; ?></code>.
-</p>
+$this->title = 'Анализ логов Nginx';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="log-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <div class="panel panel-default">
+        <div class="panel-heading">Фильтры</div>
+        <div class="panel-body">
+            <?php $form = ActiveForm::begin([
+                'method' => 'get',
+            ]); ?>
+
+            <div class="row">
+                <div class="col-md-3">
+                    <?= $form->field($searchModel, 'dateFrom')->input('date') ?>
+                </div>
+                <div class="col-md-3">
+                    <?= $form->field($searchModel, 'dateTo')->input('date') ?>
+                </div>
+                <div class="col-md-3">
+                    <?= $form->field($searchModel, 'osName')->dropDownList(
+                        Os::find()->select('name')->distinct()->indexBy('name')->column(),
+                        ['prompt' => 'Все ОС']
+                    ) ?>
+                </div>
+                <div class="col-md-3">
+                    <?= $form->field($searchModel, 'architecture')->dropDownList(
+                        ['x86' => 'x86', 'x64' => 'x64'],
+                        ['prompt' => 'Все архитектуры']
+                    ) ?>
+                </div>
+            </div>
+
+            <div class="form-group">
+                <?= Html::submitButton('Применить', ['class' => 'btn btn-primary']) ?>
+                <?= Html::a('Сбросить', ['index'], ['class' => 'btn btn-default']) ?>
+            </div>
+
+            <?php ActiveForm::end(); ?>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12">
+            <?= HighCharts::widget([
+                'clientOptions' => [
+                    'title' => ['text' => 'Запросы по датам'],
+                    'xAxis' => [
+                        'title' => ['text' => 'Ось X - дата'],
+                        'categories' => ArrayHelper::getColumn($requestsByDate, 'date'),
+                    ],
+                    'yAxis' => [
+                        'title' => ['text' => 'Ось Y - число запросов'],
+                    ],
+                    'series' => [
+                        ['name' => 'Запросы', 'data' => ArrayHelper::getColumn($requestsByDate, 'count')],
+                    ]
+                ]
+            ]); ?>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12">
+            <?php
+            $browserSeries = [];
+            $browsers = array_unique(ArrayHelper::getColumn($browserShareData, 'browser'));
+            
+            foreach ($browsers as $browser) {
+                $browserData = array_filter($browserShareData, function($item) use ($browser) {
+                    return $item['browser'] === $browser;
+                });
+                
+                $browserSeries[] = [
+                    'name' => $browser,
+                    'data' => ArrayHelper::getColumn($browserData, 'percentage'),
+                ];
+            }
+            
+            echo HighCharts::widget([
+                'clientOptions' => [
+                    'title' => ['text' => 'Доля браузеров (%)'],
+                    'xAxis' => [
+                        'title' => ['text' => 'Ось X - дата'],
+                        'categories' => array_unique(ArrayHelper::getColumn($browserShareData, 'date')),
+                    ],
+                    'yAxis' => [
+                        'title' => ['text' => 'Ось Y - % числа запросов'],
+                        'max' => 100,
+                    ],
+                    'plotOptions' => [
+                        'series' => [
+                            'stacking' => 'percent'
+                        ]
+                    ],
+                    'series' => $browserSeries,
+                ]
+            ]);
+            ?>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12">
+            <h3>Статистика по дням</h3>
+            <?php Pjax::begin(); ?>
+            <?= GridView::widget([
+                'dataProvider' => $dataProvider,
+                'columns' => [
+                    [
+                        'attribute' => 'date',
+                        'label' => 'Дата',
+                        'value' => function($model) {
+                            return Yii::$app->formatter->asDate($model['date']);
+                        }
+                    ],
+                    [
+                        'attribute' => 'count',
+                        'label' => 'Число запросов',
+                    ],
+                    [
+                        'attribute' => 'popular_url',
+                        'label' => 'Самый популярный URL',
+                    ],
+                    [
+                        'attribute' => 'popular_browser',
+                        'label' => 'Самый популярный браузер',
+                    ],
+                ],
+            ]); ?>
+            <?php Pjax::end(); ?>
+        </div>
+    </div>
+</div>
