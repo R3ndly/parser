94d11aea31e3982d774d398546de4582f47e2b67
R3ndly <akalinart020@gmail.com>
2025-08-03 20:03:03 +0800

    Обновление модели: добавил метод для получения запроса

--- models/LogSearch.php
+++ models/LogSearch.php
@@ -46,5 +46,27 @@ class LogSearch extends Model
               ->andFilterWhere(['browser.name' => $this->browserName]);
         
         return $dataProvider;
-    }
+	}
+
+	public function getQuery()
+	{
+    	$query = Log::find()
+        	->select([
+            	'DATE(request_date) as date',
+            	'COUNT(*) as count',
+            	'(SELECT path FROM url WHERE id = url_id GROUP BY url_id ORDER BY COUNT(*) DESC LIMIT 1) as popular_url',
+            	'(SELECT name FROM browser WHERE id = browser_id GROUP BY browser_id ORDER BY COUNT(*) DESC LIMIT 1) as popular_browser'
+        	])
+        	->joinWith(['os', 'browser', 'url'])
+        	->groupBy('DATE(request_date)');
+    
+    	if ($this->validate()) {
+        	$query->andFilterWhere(['>=', 'request_date', $this->dateFrom])
+            	->andFilterWhere(['<=', 'request_date', $this->dateTo])
+              	->andFilterWhere(['os.name' => $this->osName])
+              	->andFilterWhere(['os.architecture' => $this->architecture]);
+    	}
+    
+    	return $query;
+	}
 }
